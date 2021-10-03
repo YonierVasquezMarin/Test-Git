26e11c0214b66bcb64321eba4e9fdc1284522d54
Yonier Vasquez Marin <yoniervasquezmarin@gmail.com>
2021-10-03 13:11:16 -0500

    Archivos minificados

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,1 +1,1 @@
-function mostrarHola(){console.log("Hola")}console.log("Ok");for(let o=0;o<5;o++);
+function mostrarHola(){console.log("Hola")}function mostrarAdios(){console.log("Adios")}let text3="hola",text4=" yo soy Natalia",textCompleto1=text3+text4;console.log(textCompleto1);for(let o=0;o<5;o++)mostrarHola(),console.log("Sigue Funcionando");let cont=10;for(let o=0;o<cont;o++)mostrarAdios(),console.log("Funcionando");let text1="hola",text2=" yo soy Yonier",textCompleto=text1+text2;console.log(textCompleto);
